b6a473be76a4fcc9a07d327d299798217ce857a9
raiser <raisercostin@gmail.com>
2015-05-24 19:26:37 +0300

    bugs

--- src/test/java/org/raisercostin/test/cache/CacheContractTest.java
+++ src/test/java/org/raisercostin/test/cache/CacheContractTest.java
@@ -1,19 +1,25 @@
 package org.raisercostin.test.cache;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Random;
 
 import org.junit.Test;
+import org.raisercostin.test.cache.storage.MemoryStorageStrategy;
+import org.raisercostin.test.cache.strategy.LeastRecentlyUsedCacheStrategy;
+import org.raisercostin.test.cache.strategy.RandomCacheStrategy;
 
 public class CacheContractTest {
 	@Test
 	public void testBasicOperations() {
-		// SimpleCache<String, String> cache = new SimpleCache<String,
-		// String>();
-		// assertEquals(false, cache.containsKey("1"));
-		// cache.put("1", "first");
-		// assertEquals(true, cache.containsKey("1"));
-		// assertEquals("first", cache.get("1"));
+		SimpleCache<Integer, String> cacheEngine = createLRUCache();
+		cache = CacheTemplate.create(cacheEngine, null);
+		assertEquals(false, cache.containsKey("1"));
+		cache.put("1", "first");
+		assertEquals(true, cache.containsKey("1"));
+		assertEquals("first", cache.get("1"));
 	}
+
 	//
 	// @Test
 	// public void testMultipleLevels() {
@@ -24,4 +30,10 @@ public class CacheContractTest {
 	// assertEquals(true, cache.containsKey("1"));
 	// assertEquals("first", cache.get("1"));
 	// }
+
+	private SimpleCache<Integer, String> createLRUCache() {
+		return new SimpleCache<Integer, String>(
+				new MemoryStorageStrategy<Integer, String>(),
+				new LeastRecentlyUsedCacheStrategy<Integer>(5));
+	}
 }
